9da2e654efd0206afeffbd6a9c9c01dd4217c22e
Debora228 <deborabarbosa.228@gmail.com>
2018-08-04 22:23:02 -0300

    Romano e Progressão

--- 2-CondicionaisEstruturasRepetição/4-RomanoDividido5.py
+++ 2-CondicionaisEstruturasRepetição/4-RomanoDividido5.py
@@ -1,16 +1,15 @@
-romano = {'I':1,'V':5,'X':10,'L':50,'C':100,'M':1000}
-print(romano)
+romano = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+subtracao = {'IV':4,'IX':0,'XL':40,'CD':400,'CM':900}
 
 a = input()
-tam = len(a) -1
+tam = len(a)-1
+
 num = romano[a[0]]
+prim = a[1]
 
 for i in range(tam):
-    if i == tam:
-        break
-    if romano[a[i]] <= romano[a[i+1]]:
-        num = num + romano[a[i]]
-    if romano[a[i]] > romano[a[i+1]]:
-        num = num - romano[a[i]]
-
+    if prim+a[i] == subtracao.keys(i):
+      print("ok")  
+    #num = num + romano[a[i]]
+    
 print(num)
