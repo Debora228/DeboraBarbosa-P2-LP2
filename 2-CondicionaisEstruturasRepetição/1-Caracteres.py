79cc7333e74ecea3d96e234e7836faf0408cd525
Debora228 <37111986+Debora228@users.noreply.github.com>
2019-02-16 15:33:12 -0200

    Delete 1-Caracteres.py

--- 2-CondicionaisEstruturasRepetição/1-Caracteres.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-inverter = ""
-while n != 0:
-    palavra = input()
-    for i in range(n-1,-1,-1):
-        inverter = inverter + palavra[i]
-    inverter = inverter + " "
-    n = int(input())
-print (inverter) 
